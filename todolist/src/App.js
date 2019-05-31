87aed6cd45b0eff85430928912ab3ea547f0c578
Fioman123 <geym@hengdingzhineng.com>
2019-05-31 17:40:30 +0800

    Initial commit

---
+++ todolist/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// import { Component } from 'react';
+// // 等价于
+// import React from 'react';
+// const Component = React.Component;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        Hello, World!
+    </div>
+    );
+  }
+}
+
+export default App;
